ab80e71a9904b06fe4762a1de98e8a928aaad44e
Prince <princegohel915@gmail.com>
2024-02-07 15:21:28 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
+import dashboard from './dashboard';
+import addArea from './addArea';
+import showArea from './showArea';
 import './App.css';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact Component={dashboard}></Route>
+        <Route path='/addArea' Component={addArea}></Route>
+        <Route path='/showArea' Component={showArea}></Route>
+      </Routes>
+    </Router>
   );
 }
 
