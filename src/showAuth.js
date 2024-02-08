3a770774315bdeda11db93c5d22a0372e64a39fd
Mansi <mansighaskata@gmail.com>
2024-02-08 13:17:31 +0530

    addAuth.js

---
+++ src/showAuth.js
@@ -0,0 +1,268 @@
+import Header from "./includes/header";
+import Footer from "./includes/footer";
+import useScript from "./utils/useScript";
+
+function ShowAuth() {
+  useScript('/assets/bundles/echart/echarts.js');
+    return (<>
+        <Header></Header>
+        <div class="main-content">
+        <section class="section">
+          <div class="section-body">
+        <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Sortable Table</h4>
+                    <div class="card-header-action">
+                      <form>
+                        <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Search"/>
+                          <div class="input-group-btn">
+                            <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div class="card-body p-0">
+                    <div class="table-responsive">
+                      <table class="table table-striped" id="sortable-table">
+                        <thead>
+                          <tr>
+                            <th class="text-center">
+                              <i class="fas fa-th"></i>
+                            </th>
+                            <th>Task Name</th>
+                            <th>Progress</th>
+                            <th>Members</th>
+                            <th>Due Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>
+                              <div class="sort-handler">
+                                <i class="fas fa-th"></i>
+                              </div>
+                            </td>
+                            <td>Create a mobile app</td>
+                            <td class="align-middle">
+                              <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
+                                <div class="progress-bar bg-success" data-width="100"></div>
+                              </div>
+                            </td>
+                            <td>
+                              <img alt="image" src="assets/img/users/user-5.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Wildan Ahdian"/>
+                            </td>
+                            <td>2018-01-20</td>
+                            <td>
+                              <div class="badge badge-success">Completed</div>
+                            </td>
+                            <td><a href="#" class="btn btn-primary">Detail</a></td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <div class="sort-handler">
+                                <i class="fas fa-th"></i>
+                              </div>
+                            </td>
+                            <td>Redesign homepage</td>
+                            <td class="align-middle">
+                              <div class="progress" data-height="4" data-toggle="tooltip" title="40%">
+                                <div class="progress-bar" data-width="40"></div>
+                              </div>
+                            </td>
+                            <td>
+                              <img alt="image" src="assets/img/users/user-1.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Nur Alpiana"/>
+                              <img alt="image" src="assets/img/users/user-3.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Hariono Yusup"/>
+                              <img alt="image" src="assets/img/users/user-4.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Bagus Dwi Cahya"/>
+                            </td>
+                            <td>2018-04-10</td>
+                            <td>
+                              <div class="badge badge-info">Todo</div>
+                            </td>
+                            <td><a href="#" class="btn btn-primary">Detail</a></td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <div class="sort-handler">
+                                <i class="fas fa-th"></i>
+                              </div>
+                            </td>
+                            <td>Backup database</td>
+                            <td class="align-middle">
+                              <div class="progress" data-height="4" data-toggle="tooltip" title="70%">
+                                <div class="progress-bar bg-warning" data-width="70"></div>
+                              </div>
+                            </td>
+                            <td>
+                              <img alt="image" src="assets/img/users/user-1.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Rizal Fakhri"/>
+                              <img alt="image" src="assets/img/users/user-2.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Hasan Basri"/>
+                            </td>
+                            <td>2018-01-29</td>
+                            <td>
+                              <div class="badge badge-warning">In Progress</div>
+                            </td>
+                            <td><a href="#" class="btn btn-primary">Detail</a></td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <div class="sort-handler">
+                                <i class="fas fa-th"></i>
+                              </div>
+                            </td>
+                            <td>Input data</td>
+                            <td class="align-middle">
+                              <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
+                                <div class="progress-bar bg-success" data-width="100"></div>
+                              </div>
+                            </td>
+                            <td>
+                              <img alt="image" src="assets/img/users/user-2.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Rizal Fakhri"/>
+                              <img alt="image" src="assets/img/users/user-5.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Isnap Kiswandi"/>
+                              <img alt="image" src="assets/img/users/user-4.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Yudi Nawawi"/>
+                              <img alt="image" src="assets/img/users/user-1.png" class="rounded-circle" width="35"
+                                data-toggle="tooltip" title="Khaerul Anwar"/>
+                            </td>
+                            <td>2018-01-16</td>
+                            <td>
+                              <div class="badge badge-success">Completed</div>
+                            </td>
+                            <td><a href="#" class="btn btn-primary">Detail</a></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div class="settingSidebar">
+          <a href="javascript:void(0)" class="settingPanelToggle"> <i class="fa fa-spin fa-cog"></i>
+          </a>
+          <div class="settingSidebar-body ps-container ps-theme-default">
+            <div class=" fade show active">
+              <div class="setting-panel-header">Setting Panel
+              </div>
+              <div class="p-15 border-bottom">
+                <h6 class="font-medium m-b-10">Select Layout</h6>
+                <div class="selectgroup layout-color w-50">
+                  <label class="selectgroup-item">
+                    <input type="radio" name="value" value="1" class="selectgroup-input select-layout" checked/>
+                    <span class="selectgroup-button">Light</span>
+                  </label>
+                  <label class="selectgroup-item">
+                    <input type="radio" name="value" value="2" class="selectgroup-input select-layout"/>
+                    <span class="selectgroup-button">Dark</span>
+                  </label>
+                </div>
+              </div>
+              <div class="p-15 border-bottom">
+                <h6 class="font-medium m-b-10">Sidebar Color</h6>
+                <div class="selectgroup selectgroup-pills sidebar-color">
+                  <label class="selectgroup-item">
+                    <input type="radio" name="icon-input" value="1" class="selectgroup-input select-sidebar"/>
+                    <span class="selectgroup-button selectgroup-button-icon" data-toggle="tooltip"
+                      data-original-title="Light Sidebar"><i class="fas fa-sun"></i></span>
+                  </label>
+                  <label class="selectgroup-item">
+                    <input type="radio" name="icon-input" value="2" class="selectgroup-input select-sidebar" checked/>
+                    <span class="selectgroup-button selectgroup-button-icon" data-toggle="tooltip"
+                      data-original-title="Dark Sidebar"><i class="fas fa-moon"></i></span>
+                  </label>
+                </div>
+              </div>
+              <div class="p-15 border-bottom">
+                <h6 class="font-medium m-b-10">Color Theme</h6>
+                <div class="theme-setting-options">
+                  <ul class="choose-theme list-unstyled mb-0">
+                    <li title="white" class="active">
+                      <div class="white"></div>
+                    </li>
+                    <li title="cyan">
+                      <div class="cyan"></div>
+                    </li>
+                    <li title="black">
+                      <div class="black"></div>
+                    </li>
+                    <li title="purple">
+                      <div class="purple"></div>
+                    </li>
+                    <li title="orange">
+                      <div class="orange"></div>
+                    </li>
+                    <li title="green">
+                      <div class="green"></div>
+                    </li>
+                    <li title="red">
+                      <div class="red"></div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="p-15 border-bottom">
+                <div class="theme-setting-options">
+                  <label>
+                    <span class="control-label p-r-20">Mini Sidebar</span>
+                    <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input"
+                      id="mini_sidebar_setting"/>
+                    <span class="custom-switch-indicator"></span>
+                  </label>
+                </div>
+              </div>
+              <div class="p-15 border-bottom">
+                <div class="theme-setting-options">
+                  <div class="disk-server-setting m-b-20">
+                    <p>Disk Space</p>
+                    <div class="sidebar-progress">
+                      <div class="progress" data-height="5">
+                        <div class="progress-bar l-bg-green" role="progressbar" data-width="80%" aria-valuenow="80"
+                          aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <span class="progress-description">
+                        <small>26% remaining</small>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="disk-server-setting">
+                    <p>Server Load</p>
+                    <div class="sidebar-progress">
+                      <div class="progress" data-height="5">
+                        <div class="progress-bar l-bg-orange" role="progressbar" data-width="58%" aria-valuenow="25"
+                          aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <span class="progress-description">
+                        <small>Highly Loaded</small>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-4 mb-4 p-3 align-center rt-sidebar-last-ele">
+                <a href="#" class="btn btn-icon icon-left btn-primary btn-restore-theme">
+                  <i class="fas fa-undo"></i> Restore Default
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       <Footer></Footer>
+    </>);
+}
+
+export default ShowAuth;
