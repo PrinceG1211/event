d65b24f1a0cb829d3114e5451e249f7af6bbb91b
Prince <princegohel915@gmail.com>
2024-02-08 13:23:47 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import dashboard from './dashboard';
 import addArea from './addArea';
 import showArea from './showArea';
+import addAuth from './addAuth';
+import showAuth from './showAuth';
 import './App.css';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 
@@ -14,6 +16,8 @@ function App() {
         <Route path='/' exact Component={dashboard}></Route>
         <Route path='/addArea' Component={addArea}></Route>
         <Route path='/showArea' Component={showArea}></Route>
+        <Route path='/addAuth' Component={addAuth}></Route>
+        <Route path='/showAuth' Component={showAuth}></Route>  
       </Routes>
     </Router>
   );
