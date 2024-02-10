6e1f99b80574a49c9561fe657f12c7f9a50bc7d5
Harsh <harsh.s.gandhi70@gmail.com>
2024-02-10 13:26:24 +0530

    footer.js

--- src/includes/footer.js
+++ src/includes/footer.js
@@ -2,7 +2,7 @@ function Footer() {
      return(
         <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2019 <div class="bullet"></div> Design By <a href="#">Redstar</a>
+          Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="#">Event Management</a>
         </div>
         <div class="footer-right">
         </div>
