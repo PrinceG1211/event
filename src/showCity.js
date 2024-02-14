9d05663a847bc639a66447300c08c973a40d9039
Prince <princegohel915@gmail.com>
2024-02-14 13:53:59 +0530

    showCity.js

--- src/showCity.js
+++ src/showCity.js
@@ -1,6 +1,7 @@
 import Header from "./includes/header";
 import Footer from "./includes/footer";
 import useScript from "./utils/useScript";
+import { Link } from "react-router-dom";
 
 function ShowCity() {
   useScript('/assets/bundles/echart/echarts.js');
@@ -13,8 +14,8 @@ function ShowCity() {
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>Area Data</h4>
-                  <Link to="/addArea" className="btn btn-outline-primary">Add</Link>
+                  <h4>City Data</h4>
+                  <Link to="/addCity" className="btn btn-outline-primary">Add</Link>
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
@@ -22,23 +23,19 @@ function ShowCity() {
                       <thead>
                         <tr>
                           <th>No</th>
-                          <th>Area</th>
-                          <th>City</th>
-                          <th>Pincode</th>
+                          <th>CityName</th>
                           <th>Action</th>
                         </tr>
                       </thead>
                       <tbody>
                         {
-                          areaList.map((area, index) => (
+                          cityList.map((city, index) => (
                             <tr>
                               <td>{index + 1}</td>
-                              <td>{area.areaName}</td>
-                              <td>{area.cityName}</td>
-                              <td>{area.pincode}</td>
+                              <td>{city.cityName}</td>
                               <td>
-                                <Link className="btn btn-icon icon-left btn-outline-info" to={`/editArea/${area.areaID}`}><i className="far fa-edit"></i>Edit</Link><br/><br/>
-                                <button className="btn btn-icon icon-left btn-outline-danger" onClick={() => handleDelete(area.areaID)}><i className="fas fa-times"></i>Delete</button>
+                                <Link className="btn btn-icon icon-left btn-outline-info" to={`/editCity/${city.cityID}`}><i className="far fa-edit"></i>Edit</Link><br/><br/>
+                                <button className="btn btn-icon icon-left btn-outline-danger" onClick={() => handleDelete(city.cityID)}><i className="fas fa-times"></i>Delete</button>
                               </td>
                             </tr>
                           ))
